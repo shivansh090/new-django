4d2762036986d9117218387c5abec6cb928c6717
Shivansh Vikram Singh <shivanshvikramsingh764@gmail.com>
2024-10-30 10:00:40 +0530

    removed db

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
